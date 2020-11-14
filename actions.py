a19c6b7d3240a6bc0e898c56e0866a745890950d
SanderLentink.nl <sander@lent.ink>
2020-11-14 18:13:32 +0100

    debug

--- actions.py
+++ actions.py
@@ -16,12 +16,28 @@ def visualize_buttons(s):
 
 
 class Action:
+    def _add_mapping(k,v):
+        for i in range(len(k)):
+            if k[i] is '?':
+                pre = k[:i]
+                post = [i+1:]
+                self._add_mapping(pre + '0' + post)
+                self._add_mapping(pre + '1' + post)
+        if '?' not in k:
+            if k not in self._map:
+                self._map[k] = v
+            else:
+                print('WARNING duplicate key in keybinding',k)
+
     def __init__(self, mapping_file='keybindings/example.yml'):
         self._action = self._key_action
         self._memory = ''
+        self._map = {}
         with open(mapping_file, 'r') as f:
             raw = f.read()
-            self.map = load(raw)
+        for k in raw:
+
+        self.map = load(raw)
 
     def binary2action(self,inp):
         # switch between keyboard/mouse if all buttons are pressed
@@ -31,9 +47,9 @@ class Action:
             else:
                 self._action = self._key_action
         else:
-            self._action(self,inp)
+            self._action(inp)
 
-    def _key_action(self,inp):
+    def _key_action(self, inp):
         if inp in self.map:
             act = self.map[inp]
         else:
@@ -47,7 +63,7 @@ class Action:
             keyboard.send(self._memory + act['action'])
             self._memory = ''
 
-    def _mouse_action(self,inp):
+    def _mouse_action(self, inp):
         return None
 
 
