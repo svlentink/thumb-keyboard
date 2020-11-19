e4cd5a3cf5cb6736935dae5a9b403fe69427f60b
SanderLentink.nl <sander@lent.ink>
2020-11-19 21:19:56 +0100

    made private

--- actions.py
+++ actions.py
@@ -16,58 +16,58 @@ def visualize_buttons(s):
 
 
 class Action:
-    def _add_mapping(self, k, v):
+    def __add_mapping(self, k, v):
         if '?' in k:
             for i in range(len(k)):
                 if k[i] is '?':
                     pre = k[:i]
                     post = k[i+1:]
-                    self._add_mapping(pre + '0' + post, v)
-                    self._add_mapping(pre + '1' + post, v)
+                    self.__add_mapping(pre + '0' + post, v)
+                    self.__add_mapping(pre + '1' + post, v)
                     return
         else:
-            if k not in self._map:
-                self._map[k] = v
+            if k not in self.__map:
+                self.__map[k] = v
             else:
                 print('WARNING duplicate key in keybinding',k)
 
     def __init__(self, mapping_file='keybindings/example.yml'):
-        self._action = self._key_action
-        self._memory = ''
-        self._map = {}
+        self.__action = self.__key_action
+        self.__memory = ''
+        self.__map = {}
         with open(mapping_file, 'r') as f:
             raw = f.read()
         obj = load(raw)
         for k,v in obj.items():
-            self._add_mapping(k,v)
+            self.__add_mapping(k,v)
 
     def binary2action(self,inp):
         # switch between keyboard/mouse if all buttons are pressed
         if inp.count('0') == 0:
-            if self._action is self._key_action:
-                self._action = self._mouse_action
+            if self.__action is self.__key_action:
+                self.__action = self.__mouse_action
             else:
-                self._action = self._key_action
+                self.__action = self.__key_action
         else:
-            self._action(inp)
+            self.__action(inp)
 
-    def _key_action(self, inp):
-        if inp in self._map:
-            act = self._map[inp]
+    def __key_action(self, inp):
+        if inp in self.__map:
+            act = self.__map[inp]
         else:
-            self._memory = ''
+            self.__memory = ''
             print('WARNING: combination not in mapping',inp)
             return
         if 'hold' in act and act['hold']:
-            self._memory = self._memory + act['action'] + '+'
+            self.__memory = self.__memory + act['action'] + '+'
             return
         else:
-            press = self._memory + act['action']
-            self._memory = ''
+            press = self.__memory + act['action']
+            self.__memory = ''
             print('INFO pressing',press)
             keyboard.send(press)
 
-    def _mouse_action(self, inp):
+    def __mouse_action(self, inp):
         return None
 
 
